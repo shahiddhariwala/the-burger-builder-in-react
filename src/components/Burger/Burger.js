766008d3453893895db94112b201f2cea26bc82a
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-06-08 20:50:11 +0530

    if not ingredients display message to add

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,14 +3,22 @@ import classes from  './burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) =>
 {
-    const transformedIngredients =Object.keys(props.ingredients).map(igKey =>
+    let transformedIngredients =Object.keys(props.ingredients).map(igKey =>
         {
             return [...Array(props.ingredients[igKey])].map((_,index) =>
             {
                 return <BurgerIngredient type={igKey} key={igKey+index}/>
             });
-        });
-        console.log(transformedIngredients);
+        }).reduce((acc,curr)=>
+        {
+            return acc.concat(curr);
+        },[]);
+
+        
+    if(transformedIngredients.length===0)
+    {
+        transformedIngredients=(<p>Please start adding ingredeints ! <br/><span role="img" aria-label="burger">🍞+🥗+🧀+🥓+🍞 => 🍔 😋</span></p>);
+    } 
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
