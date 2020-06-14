823ec95fa30dd11b5693674873c183ec1227b6e5
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-06-15 00:55:44 +0530

    Added functionality to fetch order from firebase

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,31 @@
 import React, { Component } from "react";
 import Order from "../../components/Order/Order";
 import classes from "./orders.module.css";
+import axios from "../../axios-orders";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 class Orders extends Component {
-  state = {};
+  state = {
+    orders: [],
+    loading: true,
+  };
+  componentDidMount() {
+    axios
+      .get("/orders.json")
+      .then((res) => {
+        const fetchedOrders = [];
+        for (let key in res.data) {
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key,
+          });
+        }
+
+        this.setState({ loading: false, orders: fetchedOrders });
+      })
+      .catch((error) => {
+        this.setState({ loading: false });
+      });
+  }
   render() {
     return (
       <div className={classes.Orders}>
@@ -14,4 +37,4 @@ class Orders extends Component {
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
