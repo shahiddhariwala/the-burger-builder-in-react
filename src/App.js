c0a82933440a6c2cc0a7d8eec9a343400627ae81
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-06-14 16:30:30 +0530

    Added basic routes and routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React,{Component} from 'react';
-import Layout from './hoc/Layout/Layout';
-import BurgerBuilder  from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-class App extends Component
-{
-  render()
-  {
+import React, { Component } from "react";
+import Layout from "./hoc/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
+import { Route, Switch } from "react-router-dom";
+class App extends Component {
+  render() {
     return (
       <div>
-        <Layout> </Layout>
-        <BurgerBuilder/>
-        <Checkout/>
+        <Layout>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" component={BurgerBuilder} />
+          </Switch>
+        </Layout>
       </div>
     );
   }
