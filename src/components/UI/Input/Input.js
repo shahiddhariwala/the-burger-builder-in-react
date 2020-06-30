a24ab028dd54cc714405638f26f1437dd4b83f51
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-07-01 00:02:27 +0530

    Added Input Component

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const input = (props) => {
+  let inputElement = null;
+  switch (props.inputType) {
+    case "input":
+      inputElement = <input {...props} />;
+      break;
+    case "textarea":
+      inputElement = <textarea {...props} />;
+      break;
+    default:
+      inputElement = <input {...props} />;
+      break;
+  }
+  return (
+    <div>
+      <label>{props.label}</label>
+      {inputElement}
+    </div>
+  );
+};
+
+export default input;
